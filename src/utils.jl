d03d8428f2a6c73c32eb23de231adfa092eebd9c
chengchingwen <adgjl5645@hotmail.com>
2019-11-29 00:01:38 +0800

    bypass julia distributed issue https://github.com/JuliaLang/julia/issues/33972

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,35 @@
+#bypass julia issue #33972
+function _take_ref(rid, caller, args...)
+  rv = Distributed.lookup_ref(rid)
+    synctake = false
+    if myid() != caller && rv.synctake !== nothing
+        # special handling for local put! / remote take! on unbuffered channel
+        # github issue #29932
+        synctake = true
+        lock(rv.synctake)
+    end
+
+  v = try
+    take!(rv, args...)
+  catch e
+    if synctake
+      unlock(rv.synctake)
+    end
+    rethrow(e)
+  end
+
+  isa(v, RemoteException) && (myid() == caller) && throw(v)
+
+    if synctake
+        return Distributed.SyncTake(v, rv)
+    else
+        return v
+    end
+end
+
+_take!(c::Channel, args...) = take!(c)
+_take!(rr::RemoteChannel, args...) = Distributed.call_on_owner(_take_ref, rr, myid(), args...)
+
 @inline iscall(ex) = false
 @inline iscall(ex::Expr) = ex.head == :call
 
@@ -30,7 +62,7 @@ macro try_take!(chn, ex=nothing)
   quote
     let v
       try
-        v = take!($(esc(chn)))
+        v = _take!($(esc(chn)))
       catch e
         if isclose(e, $(esc(chn)))
           $(esc(ex))
